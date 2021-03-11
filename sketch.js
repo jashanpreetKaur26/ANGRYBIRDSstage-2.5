6e905f4a102a740f7a9267cef4d9a5a9e9d0e32f
jashanpreet <jashan2605@gmail.com>
2021-03-11 13:07:39 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
+var platform;
 
 function setup(){
     var canvas = createCanvas(1200,400);
@@ -12,7 +13,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-
+    platform =new Ground(150,300,300,170)
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -43,7 +44,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-
+   platform.display();
     box3.display();
     box4.display();
     pig3.display();
